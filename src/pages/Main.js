55cae82cd7753f0e1a55025cdd2df928420a0c7b
yeon <parkwlfod@naver.com>
2024-11-22 11:44:17 +0900

    sdf

--- src/pages/Main.js
+++ src/pages/Main.js
@@ -53,11 +53,7 @@ console.log(data)
     <div className='sortingwrap'>
     <Sorting/>
     <Flowlist data={data.top_rated}/>
-    <List title="재미난 예능 컨텐츠" data={data.tv} type="tv" />
-    <List title="인기있는 영화" data={data.movie} type="movie"/>
-    <List title="애니메이션 TOP 20" data={data.ani} type="tv"/>
-    <List title="방영중인 드라마" data={data.drama} type="tv"/>
-    <List title="아이를 위한 키즈 컨텐츠" data={data.kids} type="tv"/>
+    
     </div>
     <Scrolltopbt/>
     <Footer/>
